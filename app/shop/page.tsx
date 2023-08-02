e3299cf3a4917b594615b7fc6517b46ef0469822
ErickEpnf18 <aguilakrakatoa@gmail.com>
2023-08-02 13:57:18 -0500

    add card to shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Container from '../components/Container'
-import ShopContent from './ShopContent'
 import ClientOnly from '../components/ClientOnly'
-
+import ShopAside from './ShopAside'
+import ShopMain from './ShopMain'
 export default function page() {
     return (
         <ClientOnly>
             <Container>
-                <ShopContent />
+                <div className='flex flex-wrap md:flex-nowrap justify-start'>
+                    <div className='w-full md:w-[210px] xl:w-[232px]'>
+                        <ShopAside />
+                    </div>
+                    <div className='w-full md:w-auto xl:auto flex flex-col'>
+                        <ShopMain />
+                    </div>
+
+                </div>
+
             </Container>
         </ClientOnly>
     )
